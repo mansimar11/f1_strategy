6654cc450a78f302f5fe365ff121aa506498f2a0
unknown <mansimarsingh117@gmail.com>
2025-07-28 20:38:39 +0530

    Force add: config.py for deployment

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# "Internal Database URL"
+
+
+
+DATABASE_URL = "postgresql://f1_db_vvhh_user:IP7TNqYKKMQYFiFWrhah95KKvzOFIjf6@dpg-d22rvqh5pdvs7399vd9g-a.oregon-postgres.render.com/f1_db_vvhh"
